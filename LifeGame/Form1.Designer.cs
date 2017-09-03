90ab8fa86a838ff446b18dd72c09a87c6d8a244b
AkiraOonuki <akira313.pgm@gmail.com>
2017-09-03 16:00:08 +0900

    version1完成

--- LifeGame/Form1.Designer.cs
+++ LifeGame/Form1.Designer.cs
@@ -28,20 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonInit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(645, 41);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(627, 451);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.WordWrap = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Location = new System.Drawing.Point(645, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonInit
+            // 
+            this.buttonInit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonInit.Location = new System.Drawing.Point(645, 440);
+            this.buttonInit.Name = "buttonInit";
+            this.buttonInit.Size = new System.Drawing.Size(75, 23);
+            this.buttonInit.TabIndex = 4;
+            this.buttonInit.Text = "initialyze";
+            this.buttonInit.UseVisualStyleBackColor = true;
+            this.buttonInit.Click += new System.EventHandler(this.buttonInit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 339);
+            this.ClientSize = new System.Drawing.Size(732, 475);
+            this.Controls.Add(this.buttonInit);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStop);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "LifeGame";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonInit;
     }
 }
 
--- LifeGame/Form1.cs
+++ LifeGame/Form1.cs
@@ -12,9 +12,208 @@ namespace LifeGame
 {
     public partial class Form1 : Form
     {
+
+        private int[] map;
+        static int X_Valid = 100;
+        static int Y_Valid = 100;
+        static int X_Max = X_Valid + 2;
+        static int Y_Max = Y_Valid + 2;
+
+
         public Form1()
         {
+            map = new int[X_Max * Y_Max];
+
             InitializeComponent();
+
+            Initialyze();
+        }
+
+        public void Initialyze()
+        {
+            for (int l = 0; l < X_Max * Y_Max; ++l)
+            {
+                map[l] = 0;
+            }
+            map[(Y_Max / 2 * X_Max + X_Max / 2) - X_Max - 1] = 1;
+            map[(Y_Max / 2 * X_Max + X_Max / 2) - X_Max] = 1;
+            map[(Y_Max / 2 * X_Max + X_Max / 2) - X_Max + 1] = 1;
+            map[(Y_Max / 2 * X_Max + X_Max / 2) - 1] = 1;
+            map[(Y_Max / 2 * X_Max + X_Max / 2) + 1] = 1;
+            map[(Y_Max / 2 * X_Max + X_Max / 2) + X_Max - 1] = 1;
+            map[(Y_Max / 2 * X_Max + X_Max / 2) + X_Max] = 1;
+            map[(Y_Max / 2 * X_Max + X_Max / 2) + X_Max + 1] = 1;
+            ViewShow();
+        }
+
+        public void ViewShow()
+        {
+            string str = "";
+            for (int y = 1; y < Y_Max - 1; ++y)
+            {
+                for (int x = 1; x < X_Max - 1; ++x)
+                {
+                    int check = map[y * Y_Max + x];
+                    if (check == 1)
+                    {
+                        str += "●";
+                    }
+                    else
+                    {
+                        str += "○";
+                    }
+                }
+                str += "\r\n";
+            }
+            //string str = "";
+            //for (int y = 0; y < Y_Max; ++y)
+            //{
+            //    for (int x = 0; x < X_Max; ++x)
+            //    {
+            //        int check = map[y * Y_Max + x];
+
+            //        switch (check)
+            //        {
+            //            case 0:
+            //                str += "０";
+            //                break;
+            //            case 1:
+            //                str += "１";
+            //                break;
+            //            case 2:
+            //                str += "２";
+            //                break;
+            //            case 3:
+            //                str += "３";
+            //                break;
+            //            case 4:
+            //                str += "４";
+            //                break;
+            //            case 5:
+            //                str += "５";
+            //                break;
+            //            case 6:
+            //                str += "６";
+            //                break;
+            //            case 7:
+            //                str += "７";
+            //                break;
+            //            case 8:
+            //                str += "８";
+            //                break;
+            //            case 9:
+            //                str += "９";
+            //                break;
+            //            case 10:
+            //                str += "Ａ";
+            //                break;
+            //            case 11:
+            //                str += "Ｂ";
+            //                break;
+            //            case 12:
+            //                str += "Ｃ";
+            //                break;
+            //            case 13:
+            //                str += "Ｄ";
+            //                break;
+            //            case 14:
+            //                str += "Ｅ";
+            //                break;
+            //            case 15:
+            //                str += "Ｆ";
+            //                break;
+            //            case 16:
+            //                str += "Ｇ";
+            //                break;
+            //            default:
+            //                str += "●";
+            //                break;
+            //        }
+            //    }
+            //    str += "\r\n";
+            //}
+            textBox1.Text = str;
+        }
+
+        public void Calc()
+        {
+            for (int xl = 1; xl < X_Max - 1; ++xl)
+            {
+                map[(Y_Max + 0) * 0 + xl] = map[(Y_Max - 2) * X_Max + xl];
+            }
+
+            for (int xl = 1; xl < X_Max - 1; ++xl)
+            {
+                map[(Y_Max - 1) * X_Max + xl] = map[X_Max + xl];
+            }
+
+            for (int yl = 1; yl < Y_Max - 1; ++yl)
+            {
+                map[((X_Max - 1) * yl) + yl] = map[((X_Max - 1) * yl) + X_Max + yl - 2];
+            }
+
+            for (int yl = 1; yl < X_Max - 1; ++yl)
+            {
+                map[((X_Max - 1) * yl) + X_Max + yl - 1] = map[((X_Max) * yl) + 1];
+            }
+
+            map[0] = map[(Y_Max - 1) * X_Max - 2];
+            map[X_Max - 1] = map[(Y_Max - 2) * X_Max + 1];
+            map[(Y_Max - 1) * X_Max] = map[Y_Max * X_Max - 2];
+            map[Y_Max * X_Max - 1] = map[X_Max + 1];
+
+            for (int y = 0; y < Y_Max; ++y)
+            {
+                for (int x = 0; x < X_Max; ++x)
+                {
+                    if (x > 0 && y > 0 && x < X_Max - 1 && y < Y_Max - 1)
+                    {
+                        int check = map[y * Y_Max + x];
+                        int A = map[(y - 1) * X_Max + (x - 1)] + map[(y - 1) * X_Max + (x)] + map[(y - 1) * X_Max + (x + 1)]
+                              + map[(y) * X_Max + (x - 1)] + map[(y) * X_Max + (x + 1)]
+                              + map[(y + 1) * X_Max + (x - 1)] + map[(y + 1) * X_Max + (x)] + map[(y + 1) * X_Max + (x + 1)];
+                        if (check == 1)
+                        {
+                            if (A <= 1 || A >= 4)
+                            {
+                                map[y * Y_Max + x] = 0;
+                            }
+                        }
+                        else
+                        {
+                            if (A == 3)
+                            {
+                                map[y * Y_Max + x] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+            ViewShow();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+            timer1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonStop.Enabled = true;
+            buttonStart.Enabled = false;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Calc();
+        }
+
+        private void buttonInit_Click(object sender, EventArgs e)
+        {
+            Initialyze();
         }
     }
 }
